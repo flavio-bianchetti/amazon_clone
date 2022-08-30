2d92594d30ee3582b9060636b78784266761d197
flavio-bianchetti <fjb.developer@gmail.com>
2022-08-30 14:12:47 -0300

    feat: mongoose connection included

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
@@ -8,5 +9,15 @@ const DB = process.env.DB_CONNECTION;
 app.use(express.json());
 
 app.get('/', (_req, res) => res.status(200).send('Connection OK!'));
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(
+    () => console.log('Connected to MongoDB')
+  )
+  .catch(
+    (err) => console.error(err)
+  );
 
 app.listen(PORT, () => console.log(`connecting at port ${PORT}.`));
